7c703573e4606531eebcd3c16ab6281d93fbe22a
Sung Kim <sungKim@lawn-128-61-120-116.lawn.gatech.edu>
2016-07-21 15:49:58 -0400

    cleaner feed look. Feed includes title, source, desc for now.

--- web/src/js/feed.js
+++ web/src/js/feed.js
@@ -4,12 +4,24 @@ Copyright 2016 Sung Kim <kr.dev.sk@gmail.com>. All rights reserved.
 JSX script that renders ReactDOM for main feed.
 **/
 
+/**
+category, comments, cost, date, desc, likes, link, loc,
+pubDate, rawDate, reports, sourceID, status, title
+**/
+
 var EventPost = React.createClass({
 	render: function() {
+		var sourceID;
+		if (this.props.post.sourceID === "GTCal") {
+			sourceID = <div className="sourceId">{this.props.post.category}</div>;
+		} else {
+			sourceID = <div className="sourceId">{this.props.post.sourceID}</div>;
+		}
 		return (
 			<div className="eventPost">
-				<h4>{this.props.post.title}</h4>
-				<p>{this.props.post.desc}</p>
+				<div className="eventTitle">{this.props.post.title}</div>
+				{sourceID}
+				<div className="eventDesc">{this.props.post.desc}</div>
 			</div>
 		);
 	}
