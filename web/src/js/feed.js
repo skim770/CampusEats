b5c2d2507bfd0b9967fedd5af6e9f8ba4e615c92
Sung Kim <sungKim@Sungs-MacBook.local>
2016-07-21 19:45:37 -0400

    feed only shows only events today and in future. Date headers added.

--- web/src/js/feed.js
+++ web/src/js/feed.js
@@ -9,6 +9,23 @@ category, comments, cost, date, desc, likes, link, loc,
 pubDate, rawDate, reports, sourceID, status, title
 **/
 
+var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// Extract and create today's yyyy-mm-dd manually, otherwise
+// todayStamp will take into account seconds/milliseconds.
+var today = new Date();
+var todayStamp = today.getFullYear() + "-";
+if (today.getMonth() + 1 < 10) {
+	todayStamp += "0";
+}
+todayStamp += (today.getMonth() + 1) + "-";
+if (today.getDate() < 10) {
+	todayStamp += "0";
+}
+todayStamp += today.getDate();
+var prevDateHeader = Date.parse(todayStamp);
+
 var EventPost = React.createClass({
 	render: function() {
 		var sourceID;
@@ -17,13 +34,32 @@ var EventPost = React.createClass({
 		} else {
 			sourceID = <div className="sourceId">{this.props.post.sourceID}</div>;
 		}
-		return (
-			<div className="eventPost">
-				<div className="eventTitle">{this.props.post.title}</div>
-				{sourceID}
-				<div className="eventDesc">{this.props.post.desc}</div>
-			</div>
-		);
+
+		var post;
+		var eventDay = Date.parse(this.props.post.rawDate.substring(0, 10));
+		var dateHeader = <div className="dateHeader">{eventDay}</div>;
+		if (eventDay > prevDateHeader) {
+			post = (
+				<div>
+				<DateHeader day={eventDay} />
+				<div className="eventPost">
+					<div className="eventTitle">{this.props.post.title}</div>
+					{sourceID}
+					<div className="eventDesc">{this.props.post.desc}</div>
+				</div>
+				</div>
+			);
+			prevDateHeader = eventDay;
+		} else {
+			post = (
+				<div className="eventPost">
+					<div className="eventTitle">{this.props.post.title}</div>
+					{sourceID}
+					<div className="eventDesc">{this.props.post.desc}</div>
+				</div>
+			);
+		}
+		return post;
 	}
 });
 
@@ -36,7 +72,29 @@ var EventFeed = React.createClass({
 		});
 		return (
 			<div className="eventFeed">
+				<DateHeader day="Today" />
 				{postNodes}
+			</div>
+		);
+	}
+});
+
+var DateHeader = React.createClass({
+	render: function() {
+		var dayString = "";
+		var day = new Date(this.props.day);
+		day.setTime(day.getTime() + (1000 * 60 * 60 * 24));
+		if (this.props.day === "Today") {
+			day = new Date();
+			dayString = "Today - ";
+		}
+		dayString += dayOfWeek[day.getDay()] 
+			+ ", " + monthOfYear[day.getMonth()] 
+			+ " " + (day.getDate())
+			+ ", " + day.getFullYear();
+		return (
+			<div className="dateHeader">
+				{dayString}
 			</div>
 		);
 	}
@@ -52,7 +110,10 @@ function renderDOM(posts) {
 postsRef.orderByChild('rawDate').on('value', function(snapshot) {
 	var posts = [];
 	snapshot.forEach(function(post) {
-		posts.push(post.val());
+		var eventDay = post.child("rawDate").val().substring(0, 10);
+		if (Date.parse(eventDay) >= Date.parse(todayStamp)) {
+			posts.push(post.val());
+		}
 	});
 	renderDOM(posts);
 });
