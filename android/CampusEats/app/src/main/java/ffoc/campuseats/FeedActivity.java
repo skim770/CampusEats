b1ec9cea7a5ee140dd350c2bc3d16a39c5a6757e
Johnston, William Allen Jr <wjohnston9@gatech.edu>
2016-07-21 17:45:36 -0400

    Feed displays first 50 entries in text form
    still need to make it look pretty

--- android/CampusEats/app/src/main/java/ffoc/campuseats/FeedActivity.java
+++ android/CampusEats/app/src/main/java/ffoc/campuseats/FeedActivity.java
@@ -1,8 +1,10 @@
 package ffoc.campuseats;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -27,8 +29,11 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        final LinearLayout linearLayout = (LinearLayout)findViewById(R.id.feedLayout);
 
-        final TextView feedText   = (TextView) findViewById(R.id.feedDisplay);
+        final TextView[] textViews = new TextView[50];
+        final TextView newText   = new TextView(this);
+        final Context context = this;
         //DatabaseReference ref = database.getReference();
         //DataSnapshot snapshot = new DataSnapshot(ref);
         ValueEventListener valueEventListener = new ValueEventListener() {
@@ -39,7 +44,8 @@ public class FeedActivity extends AppCompatActivity {
                 //DataSnapshot iteration = snap.iterator().next();
                 long childCount = dataSnapshot.getChildrenCount();
 
-                for(long i = 0; i < childCount; i++){
+                for(int i = 0; i < 50; i++){
+                    TextView feedText = new TextView(context);
                     DataSnapshot iteration = snap.iterator().next();
 
                     String str = iteration.child("title").getValue().toString();
@@ -48,10 +54,10 @@ public class FeedActivity extends AppCompatActivity {
                     str = iteration.child("date").getValue().toString();
                     feedText.append("\n" + "Date: " + str);
 
-                    str = iteration.child("location").getValue().toString();
+                    str = iteration.child("loc").getValue().toString();
                     feedText.append("\n" + "Location: " + str);
 
-                    str = iteration.child("description").getValue().toString();
+                    str = iteration.child("desc").getValue().toString();
                     feedText.append("\n" + "Description: " + str);
 
                     str = iteration.child("likes").getValue().toString();
@@ -61,6 +67,12 @@ public class FeedActivity extends AppCompatActivity {
                     feedText.append("\n" + "Status: " + str);
 
                     feedText.append("\n\n");
+
+                    //feedText.setId(i);
+
+                    textViews[i] = feedText;
+
+                    linearLayout.addView(textViews[i]);
                 }
 
             }
@@ -71,6 +83,10 @@ public class FeedActivity extends AppCompatActivity {
             }
         };
         queryRef.addListenerForSingleValueEvent(valueEventListener);
+
+        if(textViews[0] != null) {
+            linearLayout.addView(textViews[0]);
+        }
         //feedText.setText(queryRef.toString());
 
 
